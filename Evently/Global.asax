9d7aad6ac2f2d3081d775228d766b3021a47166b
shareDemCodez <shabari.cs@gmail.com>
2017-03-26 17:58:15 -0400

    Initial Setup - changing the bundle config to have the jquery, css in one script - moving the navbar to its own partial view - adding the layout for all pages

---
+++ Evently/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Evently.MvcApplication" Language="C#" %>
---
+++ Evently/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Evently.Startup))]
+namespace Evently
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
